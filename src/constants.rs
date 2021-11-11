594fa09481b152750749699a59a0c0dd1979dca0
furetur <furetur@gmail.com>
2021-11-11 12:09:06 +0300

    increased compiler and exec timeouts

--- src/constants.rs
+++ src/constants.rs
@@ -4,5 +4,5 @@ pub const COMPILED_CODE_DATA_FOLDER: &str = "compiled";
 pub const JEX_COMPILER_FILE: &str = "JexCompiler-0.2.jar";
 pub const JEX_VM_FILE: &str = "jex_vm-0.4";
 
-pub const JEX_COMPILE_TIMEOUT: u64 = 2;
-pub const JEX_EXEC_TIMEOUT: u64 = 5;
+pub const JEX_COMPILE_TIMEOUT: u64 = 10;
+pub const JEX_EXEC_TIMEOUT: u64 = 10;
