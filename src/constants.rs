893bd0709bd23bd51423799ae9ea0175ada92ae5
furetur <furetur@gmail.com>
2021-11-18 22:42:07 +0300

    updated jex compiler to 0.3

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
 pub const RUNTIME_DATA_FOLDER: &str = "runtime_data";
 pub const SOURCE_CODE_DATA_FOLDER: &str = "source_code";
 pub const COMPILED_CODE_DATA_FOLDER: &str = "compiled";
-pub const JEX_COMPILER_FILE: &str = "JexCompiler-0.2.jar";
+pub const JEX_COMPILER_FILE: &str = "JexCompiler-0.3.jar";
 pub const JEX_VM_FILE: &str = "jex_vm-0.4";
 
 pub const JEX_COMPILE_TIMEOUT: u64 = 10;
